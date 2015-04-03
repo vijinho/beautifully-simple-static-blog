bdfb8a68b49acb130ddbf813f23d1433815fcd26
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-04-03 14:09:51 +0100

    remove bad lines

--- cli.py
+++ cli.py
@@ -9,12 +9,3 @@ import click
 
 if __name__ == '__main__':
     pass
-
-    data = {'body_title': CONFIG['title'] + ' / ' + filename[:-5],
-            'head_title': filename[:-5],
-            'head_author': CONFIG['author'],
-            'head_keywords': filename[:-5] + ' file',
-            'head_description': filename[:-5 + ' for website ' + CONFIG['title'],
-            'blog_posts_meta': get_blog_posts_meta(),
-            'body_content': parse_markdown_file('docs/' + filename[:-3])}
-
